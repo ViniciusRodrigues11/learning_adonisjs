06b43e4ff5466b160344b30ee7cb64a5253eccc5
unknown <vrsstudy@gmail.com>
2022-02-13 12:05:16 -0400

    first commit

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,8 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UsersController {
+    public async create({request}: HttpContextContract){
+        const data = request.only(["name", "username"])
+        console.log(data)
+    }
+}
